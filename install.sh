43635744760aa730a51f7c29e20ee468dc837564
Only Coder <only.coder000@gmail.com>
2023-04-20 20:50:19 +0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt install xfce4-panel xfdesktop4 xfwm4 xfce4-settings xfce4-session tigervnc-standalone-server firefox nmap;
+echo "#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &" > ~/.vnc/xstartup;
+chmod -R 777 ~/.vnc/;
+vncserver;
+nmap localhost;
+
